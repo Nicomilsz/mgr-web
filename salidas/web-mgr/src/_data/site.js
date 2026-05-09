3200ab7a42fb354bcd2f34e7f89bcdcd7362b0f9
Nico Milsz <nicomilsz@Mac.fibertel.com.ar>
2026-05-09 20:27:26 -0300

    Agrega modos del catalogo y actualiza copy profesional

--- salidas/web-mgr/src/_data/site.js
+++ salidas/web-mgr/src/_data/site.js
@@ -1,9 +1,10 @@
 module.exports = {
   wa: {
     number: "5491149808910",
-    msgGeneral: "Hola%2C%20quisiera%20consultar%20por%20una%20propiedad%20en%20CABA",
-    msgTasacion: "Hola%2C%20quiero%20tasar%20mi%20propiedad%20en%20CABA",
-    msgBusqueda: "Hola%2C%20estoy%20buscando%20una%20propiedad%20en%20CABA",
+    msgGeneral:  "Hola%2C%20quiero%20hacer%20una%20consulta%20sobre%20MGR%20Inmobiliaria.",
+    msgTasacion: "Hola%2C%20quiero%20solicitar%20una%20tasaci%C3%B3n%20profesional%20para%20vender%20una%20propiedad%20en%20CABA.",
+    msgVendedor: "Hola%2C%20quiero%20hablar%20sobre%20una%20propiedad%20que%20estoy%20evaluando%20vender.",
+    msgBusqueda: "Hola%2C%20estoy%20buscando%20una%20propiedad%20en%20CABA.",
   },
   instagram: "mgr_inmobiliaria",
   email: "contacto@mgr.com.ar",
